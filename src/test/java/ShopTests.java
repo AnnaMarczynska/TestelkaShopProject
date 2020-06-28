c2f11e24136297d14810aa198aa40b76769a3705
anna.marczynska <anna.m.marczynska@gmail.com>
2020-06-28 23:07:20 +0200

    Corrected addProductToCartTest Add addingManyProductsToCart Add addingManyItemsOfProductToCart

--- src/test/java/ShopTests.java
+++ src/test/java/ShopTests.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.Keys;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,16 +17,18 @@ public class ShopTests {
 
     WebDriver driver;
     WebDriverWait wait;
+    Actions action;
 
     @BeforeEach
     public void driverSetUp(){
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.navigate().to("https://fakestore.testelka.pl/");
 
         wait = new WebDriverWait(driver, 10);
+        action = new Actions(driver);
 
         //hide info about demo page
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("a[class=\"woocommerce-store-notice__dismiss-link\"]"))));
@@ -63,8 +67,57 @@ public class ShopTests {
         //by going to cart
         driver.findElement(By.cssSelector("div[class=\"woocommerce-message\"]>a")).click();
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("td[class=\"product-remove\"]"))));
-        Assertions.assertFalse(driver.findElement(By.cssSelector("article[id=\"post-6\"]")).getText().contains(productName),
+        Assertions.assertTrue(driver.findElement(By.cssSelector("article[id=\"post-6\"]")).getText().contains(productName),
                 "Product " + productName + " was not add to the cart");
+    }
+
+    @Test
+    public void addingManyProductsToCart(){
+        //go to product page by clicking the link
+        driver.findElement(By.xpath(".//h2[contains(text(), \"Yoga i pilates\")]")).click();
+
+        //adding fist product on page to cart
+        driver.findElement(By.cssSelector("a[data-product_id=\"61\"]")).click();
+
+        //adding third product on page to cart
+        driver.findElement(By.cssSelector("a[data-product_id=\"60\"]")).click();
+
+        //giving page time to refresh after adding products to cart
+        wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector("a>span"))));
+
+        //moving to cart page
+        driver.findElement(By.className("cart-contents")).click();
+
+        //getting number of products in cart
+        int numberOfProductsInCart = driver.findElements(By.xpath(".//td[@class=\"product-name\"]")).size();
+
+        Assertions.assertTrue(numberOfProductsInCart>1, "Number of products in cart is lower than 2");
+    }
+
+    @Test
+    public void addingManyItemsOfProductToCart(){
+        //go to product page by clicking the link
+        driver.findElement(By.xpath(".//h2[contains(text(), \"Yoga i pilates\")]")).click();
+
+        //adding fist product on page to cart
+        driver.findElement(By.cssSelector("a[data-product_id=\"61\"]")).click();
+
+        //giving page time to refresh after adding products to cart
+        wait.until(ExpectedConditions.stalenessOf(driver.findElement(By.cssSelector("a>span"))));
+
+        //moving to cart page
+        driver.findElement(By.className("cart-contents")).click();
+
+        //adding another item to cart from cart page
+        driver.findElement(By.cssSelector("input[type=\"number\"]")).sendKeys(Keys.ARROW_UP);
+
+        driver.findElement(By.cssSelector("button[name=\"update_cart\"]")).click();
+
+        //getting number of items of product in cart
+        WebElement productInCart = driver.findElement(By.cssSelector("input[type=\"number\"]"));
+        int numberOfItems = Integer.parseInt(productInCart.getAttribute("value"));
+        Assertions.assertTrue(numberOfItems>1, "Number of items of product is lower than 2");
+
     }
 
 }
